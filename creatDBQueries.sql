5b9771f4aaeb4098785f1b81ded22d6c78b7cfcd
MostafaKadry <mostafakadry806@gmail.com>
2024-12-15 19:37:21 +0200

    finishing DDL queries

--- creatDBQueries.sql
+++ creatDBQueries.sql
@@ -40,8 +40,8 @@ create table out_source_servise(
 id int primary key,
 outsurce_ID int,
 department_ID int,
-servece_name varchar(20),
-serviceStartTime datetime
+srvs_name varchar(20),
+srvs_datetime datetime
 )
 ------------------------------------------
 create table bank_treasury(
@@ -81,7 +81,7 @@ birthdate date,
 gender char(1),
 )
 ------------------------------------------
-create table phone(
+create table cust_phone(
 customer_id int,
 phone_num varchar
 )
@@ -100,3 +100,71 @@ time_out datetime,
 )
 -----------------------------------------
 create table parking (area int)
+-----------------------------------------
+create table dependant (
+emp_id int,
+dep_name varchar(25),
+relation varchar(25),
+BD date,
+)
+-----------------------------------------
+create table emp_phone (
+emp_id int,
+phone_num varchar
+)
+-----------------------------------------
+create table customer_adderss (
+cust_id int,
+zip_code int,
+street varchar(10),
+build_number int
+)
+-----------------------------------------
+create table zip_codes(
+city varchar(15),
+zip_code int
+)
+-----------------------------------------
+create table transMoneyTransaction (
+to_account_id int,
+from_account_id int,
+transDateandTime datetime,
+transaction_amount money,
+rans_id int,
+trans_by int default(0)
+)
+-----------------------------------------
+create table account(
+id int primary key,
+acc_type varchar(10),
+balance money,
+cust_id int,
+acc_password varchar(30)
+)
+-----------------------------------------
+create table withdraw_and_deposite(
+id int primary key,
+amount money,
+wd_date datetime,
+)
+-----------------------------------------
+create table ATM (
+id int primary key,
+atm_location varchar(20),
+balance money
+)
+------------------------------------------
+create table refull_atm (
+atm_id int,
+outsource_id int,
+amount money,
+dateOfRfull datetime
+)
+------------------------------------------
+create table out_source_agency (
+id int primary key,
+name varchar(20)
+)
+------------------------------------------
+--- we have 24 table, but in mapping there are only 23, I think the increased one is in normalization.
+------------------------------------------
