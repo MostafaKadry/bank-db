18f4f3999b3da655a95cf1dddff10d13f0f828ee
Elmokhtar Ahmed Abdelghane <74218720+mokhtarjo21@users.noreply.github.com>
2024-12-16 22:27:17 +0200

    Update constraint.sql

--- constraint.sql
+++ constraint.sql
@@ -1,3 +1,4 @@
+use bankSysDb; 
 alter table Employee
 add constraint super foreign key(super_id)  references Employee(id),
 constraint emp_gender check( gender in( 'f','m' ));
@@ -19,8 +20,8 @@ add constraint equp_dept foreign key(depart_id)  references department(id);
 -------------------------------------------------------------------------------
 alter table out_source_servise
 add constraint out_servive1 foreign key(department_ID)  references department(id),
-constraint out_servive foreign key(outsurce_ID)  references out_source_agency(id);
-
+constraint out_servive foreign key(outsurce_ID)  references out_source_agency(id),
+constraint treasuryid foreign key(treasury_id)  references bank_treasury(id);
 -------------------------------------------------------------------------------
  alter table loan
 add constraint loan_bank foreign key(treasury_id)  references bank_treasury(id);
@@ -64,7 +65,8 @@ constraint atm_id foreign key(trans_by)  references ATM(id);
 ----------------------------------------------------------------------------------
  alter table withdraw_and_deposite
 add constraint acc foreign key(account_id)  references account(id),
-constraint f_atm_id foreign key(action_by)  references ATM(id);
+constraint f_atm_id foreign key(action_by)  references ATM(id),
+constraint wdteasury foreign key(treasury_id)  references bank_treasury(id);
 -------------------------------------------------------------------------------
  alter table account
 add constraint custac_id foreign key(cust_id)  references customer(id),
@@ -77,3 +79,7 @@ add constraint cust_id foreign key(outsource_id)  references out_source_agency(i
 -------------------------------------------------------------------------------
  alter table car
 add constraint owcar check( owner_type in( 'employee','customer' ));
+----------------------------------------------------------------------------------------
+ alter table parking_time
+add constraint carid foreign key(car_id)  references car(car_id),
+constraint areaid foreign key(area_id)  references parking(area);
