2383d47ba66564379777d05a705a663e1c9074c6
MostafaKadry <mostafakadry806@gmail.com>
2024-12-16 18:54:39 +0200

    add and fix constrain bugs

---
+++ constraint.sql
@@ -0,0 +1,79 @@
+alter table Employee
+add constraint super foreign key(super_id)  references Employee(id),
+constraint emp_gender check( gender in( 'f','m' ));
+
+-------------------------------------------------------------------------------
+alter table emp_working_depart
+add constraint empforw foreign key(emp_id)  references Employee(id),
+constraint deptforw foreign key(dept_id)  references department(id),
+constraint compwork primary key(emp_id,dept_id);
+
+-------------------------------------------------------------------------------
+alter table department
+add constraint mang_dept foreign key(manger_id)  references Employee(id);
+
+-------------------------------------------------------------------------------
+alter table equipment
+add constraint equp_dept foreign key(depart_id)  references department(id);
+
+-------------------------------------------------------------------------------
+alter table out_source_servise
+add constraint out_servive1 foreign key(department_ID)  references department(id),
+constraint out_servive foreign key(outsurce_ID)  references out_source_agency(id);
+
+-------------------------------------------------------------------------------
+ alter table loan
+add constraint loan_bank foreign key(treasury_id)  references bank_treasury(id);
+
+
+-----------------------------------------ssss--------------------------------------
+alter table installment
+add constraint loankey foreign key(loan_id)  references loan(id),
+constraint install_id primary key(loan_id,customer_id, payment_date),
+constraint custkey foreign key(customer_id)  references customer(id);
+
+-------------------------------------------------------------------------------
+alter table takeLoan
+add constraint loankeytake foreign key(loan_id)  references loan(id),
+constraint custkeytake foreign key(customer_id)  references customer(id),
+constraint take_loan primary key(loan_id,customer_id);
+-------------------------------------------------------------------------------
+alter table customer
+add constraint cust_gender check( gender in( 'f','m' ));
+
+-------------------------------------------------------------------------------
+ alter table cust_phone
+add constraint custphonekey foreign key(customer_id)  references customer(id);
+-------------------------------------------------------------------------------
+ alter table dependant
+ add constraint depend_id foreign key(emp_id)  references employee(id);
+-------------------------------------------------------------------------------
+alter table emp_phone
+add constraint empphonekey foreign key(emp_id)  references employee(id);
+-------------------------------------------------------------------------------
+alter table customer_adderss
+add constraint custaddress foreign key(cust_id)  references customer(id),
+constraint zcode foreign key (zip_code) references zip_codes(zip_code);
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+alter table transMoneyTransaction
+add constraint toacc foreign key(to_account_id)  references account(id),
+constraint fromacc foreign key(from_account_id)  references account(id),
+constraint atm_id foreign key(trans_by)  references ATM(id);
+----------------------------------------------------------------------------------
+ alter table withdraw_and_deposite
+add constraint acc foreign key(account_id)  references account(id),
+constraint f_atm_id foreign key(action_by)  references ATM(id);
+-------------------------------------------------------------------------------
+ alter table account
+add constraint custac_id foreign key(cust_id)  references customer(id),
+constraint account_type check( acc_type in( 'gold','silver','platinum' ));
+
+-------------------------------------------------------------------------------
+ alter table refull_atm
+add constraint cust_id foreign key(outsource_id)  references out_source_agency(id);
+
+-------------------------------------------------------------------------------
+ alter table car
+add constraint owcar check( owner_type in( 'employee','customer' ));
--- creatDBQueries.sql
+++ creatDBQueries.sql
@@ -1,170 +1,174 @@
+drop database bankSysDb;
 create database bankSysDb;
-use bankSysDb;
+use bankSysDb; 
 -- 1----------------------------------------
 create table Employee (
 id int primary key,
-super_id int,
-salary money,
+super_id int not null,
+salary money default 0,
 position varchar(15),
 f_name varchar(10),
 l_name varchar(10),
 gender char(1),
 birth_date date,
 adress varchar(20)
-)
+);
 -- 2----------------------------------------
 create table emp_working_depart (
-emp_id int,
-dept_id int, 
+emp_id int not null,
+dept_id int not null, 
 startDate date
-)
+);
 ------------------------------------------
 create table department (
 id int primary key,
 dep_name varchar(20),
 dep_location varchar(20),
-manger_id int
-)
+manger_id int not null
+);
 ------------------------------------------
 
 create table equipment(
 id int primary key,
-depart_id int,
+depart_id int not null,
 equp_name varchar(15),
 equp_type varchar(15),
 starting_use date
-)
+);
 ------------------------------------------
 
 create table out_source_servise(
 id int primary key,
-outsurce_ID int,
-department_ID int,
+outsurce_ID int not null,
+department_ID int not null,
 srvs_name varchar(20),
 srvs_datetime datetime
-)
+);
 ------------------------------------------
 create table bank_treasury(
 id int primary key,
-balance money
-)
+balance money default 0
+);
 ------------------------------------------
 create table loan (
 id int primary key,
 duration varchar(10),
 startDate date,
 endDate date,
-amount money,
+amount money default 0,
 mang_fees money,
-treasury_id int
-)
-------------------------------------------
+treasury_id int not null
+);
+--------------------------sss----------------
 create table installment (
-loan_id int,
-customer_id int,
-payment_date datetime,
-paymentAmount money
+loan_id int not null,
+customer_id int not null,
+payment_date datetime default getdate() not null,
+paymentAmount money default 0
 )
 -----------------------------------------
 create table takeLoan (
-loan_id int,
-customer_id int,
+loan_id int not null,
+customer_id int not null,
 takeing_loan datetime
-)
+);
 ------------------------------------------
 create table customer (
 id int primary key,
-super_id int,
 f_name varchar(10),
 l_name varchar(10),
 birthdate date,
 gender char(1),
-)
+);
 ------------------------------------------
 create table cust_phone(
-customer_id int,
-phone_num varchar
-)
+customer_id int not null,
+phone_num varchar primary key,
+);
 -----------------------------------------
 create table car (
-id int,
-owner_id int,
-owner_type varchar(10)
+car_id int,
+owner_id int not null,
+owner_type varchar(10),
+PRIMARY KEY (car_id, owner_type, owner_id)
 )
 ------------------------------------------
 create table parking_time (
-car_id int,
-area_id int,
+car_id int not null,
+area_id int not null,
 time_in datetime,
 time_out datetime,
-)
+);
 -----------------------------------------
-create table parking (area int)
+create table parking (area int primary key)
 -----------------------------------------
 create table dependant (
-emp_id int,
+emp_id int not null,
 dep_name varchar(25),
 relation varchar(25),
 BD date,
-)
+);
 -----------------------------------------
 create table emp_phone (
-emp_id int,
-phone_num varchar
-)
+emp_id int not null,
+phone_num varchar primary key
+);
 -----------------------------------------
 create table customer_adderss (
-cust_id int,
-zip_code int,
+cust_id int not null,
+zip_code int not null,
 street varchar(10),
 build_number int
-)
+);
 -----------------------------------------
-create table zip_codes(
-city varchar(15),
-zip_code int
-)
+create table zip_codes (
+zip_code int primary key,
+city varchar(15)
+);
 -----------------------------------------
 create table transMoneyTransaction (
-to_account_id int,
-from_account_id int,
+to_account_id int not null,
+from_account_id int not null,
 transDateandTime datetime,
 transaction_amount money,
-rans_id int,
+rans_id int primary key,
 trans_by int default(0)
-)
+);
 -----------------------------------------
 create table account(
 id int primary key,
 acc_type varchar(10),
-balance money,
-cust_id int,
+balance money default 0,
+cust_id int not null,
 acc_password varchar(30)
-)
+);
 -----------------------------------------
 create table withdraw_and_deposite(
 id int primary key,
 amount money,
 wd_date datetime,
-)
+wd_type varchar(10) ,
+account_id int not null,
+action_by int default 0
+);
 -----------------------------------------
 create table ATM (
 id int primary key,
 atm_location varchar(20),
-balance money
-)
+balance money default 0 
+);
 ------------------------------------------
 create table refull_atm (
-atm_id int,
-outsource_id int,
-amount money,
+atm_id int not null,
+outsource_id int not null,
+amount money default 0,
 dateOfRfull datetime
-)
+);
 ------------------------------------------
 create table out_source_agency (
 id int primary key,
 name varchar(20)
-)
+);
 ------------------------------------------
 --- we have 24 table, but in mapping there are only 23, I think the increased one is in normalization.
 ------------------------------------------
